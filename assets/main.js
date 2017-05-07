4b4e93d4da755c862aeb210b3cdd6f27eb38b2a1
caroquintana <caro.quintana.n@gmail.com>
2017-05-07 01:34:01 -0400

    Añadiendo el título

--- assets/main.js
+++ assets/main.js
@@ -15,3 +15,24 @@ var anayn = new MiembroSquad("Anayn Pavez", 27, "Dibujar, Pastelería, Andar en 
 var anais = new MiembroSquad("Anais Araya",20, "Estar con Amelia, Ver Netflix, Estar con mi pareja");
 var simona = new MiembroSquad("Simona Ulloa", 22, "Ver Netflix, Escuchar música, Tomar chelas");
 var dulce = new MiembroSquad("Dulce Frau", 25, "Cocinar, Leer, Subir montañas");
+
+//Añadiendo un título
+var elemento = document.createElement("h2");
+var contenido = document.createTextNode("A-TÓ-MI-CO Squad");
+elemento.appendChild(contenido);
+elemento.setAttribute("align", "center");
+document.getElementById("titulo").appendChild(elemento);
+
+
+
+
+
+
+
+
+
+//1.Crear el elemento
+//2.Crear un nodo de texto
+//3.Añadir el nodo de texto al elemento
+//4.Agregar atributos al elemento
+//5.Agregar el elemento al documento
