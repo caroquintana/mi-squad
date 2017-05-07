e11246a2c79971a578ba44558389ac60b876a27e
caroquintana <caro.quintana.n@gmail.com>
2017-05-07 17:36:24 -0400

    divs con contenidos

--- assets/main.js
+++ assets/main.js
@@ -29,7 +29,14 @@ document.getElementById("titulo").appendChild(elemento);
 //Añadiendo a cada div los datos
 
 
-document.getElementById("carolinaq").innerHTML = "Nombre Completo: " + carolina.nombreCompleto + "<br> Edad: " + carolina.edad + " años" + "<br> Hobbies: " + carolina.hobbies; 
+var divcarolina = document.getElementById("carolinaq").innerHTML = "Nombre Completo: " + carolina.nombreCompleto + "<br> Edad: " + carolina.edad + " años" + "<br> Hobbies: " + carolina.hobbies; 
+var divjosefa = document.getElementById("josefah").innerHTML = "Nombre Completo: " + josefa.nombreCompleto + "<br> Edad: " + josefa.edad + " años" + "<br> Hobbies: " + josefa.hobbies; 
+var divmaibett = document.getElementById("maibeett").innerHTML = "Nombre Completo: " + maibeet.nombreCompleto + "<br> Edad: " + maibeet.edad + " años" + "<br> Hobbies: " + maibeet.hobbies; 
+var divmuriel = document.getElementById("murielb").innerHTML = "Nombre Completo: " + muriel.nombreCompleto + "<br> Edad: " + muriel.edad + " años" + "<br> Hobbies: " + muriel.hobbies; 
+var divanayn = document.getElementById("anaynp").innerHTML = "Nombre Completo: " + anayn.nombreCompleto + "<br> Edad: " + anayn.edad + " años" + "<br> Hobbies: " + anayn.hobbies; 
+var divanais = document.getElementById("anaisa").innerHTML = "Nombre Completo: " + anais.nombreCompleto + "<br> Edad: " + anais.edad + " años" + "<br> Hobbies: " + anais.hobbies; 
+var divsimona = document.getElementById("simonau").innerHTML = "Nombre Completo: " + simona.nombreCompleto + "<br> Edad: " + simona.edad + " años" + "<br> Hobbies: " + simona.hobbies; 
+var divdulce = document.getElementById("dulcef").innerHTML = "Nombre Completo: " + dulce.nombreCompleto + "<br> Edad: " + dulce.edad + " años" + "<br> Hobbies: " + dulce.hobbies; 
 
 
 
